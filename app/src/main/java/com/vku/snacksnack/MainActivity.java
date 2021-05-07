4ca1ed3518fa0504362c18070f77e634695254ac
Lê Vũ Ngọc Huy <lvnhuy.19it2@vku.udn.vn>
2021-05-07 21:24:15 +0700

    DoAnCoSo3

---
+++ app/src/main/java/com/vku/snacksnack/MainActivity.java
@@ -0,0 +1,22 @@
+package com.vku.snacksnack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference reference = firebaseDatabase.getReference("Message");
+
+        reference.setValue("Hello, World");
+    }
+}
